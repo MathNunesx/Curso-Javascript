0cff0f89e7fb0521e44bf5d0b768b35294cbce9a
Matheus Nunes <matheusjobs04@gmail.com>
2024-07-03 16:46:22 -0300

    aula 08

---
+++ Aula007/aula07.js
@@ -0,0 +1,10 @@
+// IF E ELSE
+
+let num=5
+
+if(num > 10){
+    console.log("numero maior que 10")
+}else{
+    console.log("numero menor que 10")
+}
+console.log("fim do programa")
---
+++ Aula008/aula08.js
@@ -0,0 +1,29 @@
+//FUNÇÕES
+
+// function nome(){
+//     console.log("roi")
+// }
+
+// nome() // -> chamada da função
+
+// function soma(){
+//     let n1 = 1;
+//     let n2 = 4;
+//     let soma = n1 + n2;
+//     console.log(soma);
+// }
+
+// for(let i=0; i < 4; i++){
+//     soma()
+// }
+
+function mudarTexto(){
+    let d1=document.getElementById("d1");
+    let d2=document.getElementById("d2");
+    let d3=document.getElementById("d3");
+
+    d1.innerHTML="hello";
+    d2.innerHTML="hello";
+    d3.innerHTML="hello";
+}
+
