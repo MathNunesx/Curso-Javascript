19bd9fd8f2be9299bf76418d1279707c1866e96b
Matheus Nunes <matheusjobs04@gmail.com>
2024-07-08 18:35:16 -0300

    aula 10

---
+++ CursoJavascript/Aula010/aula10.js
@@ -0,0 +1,18 @@
+// Funções parametrizadas
+
+// function soma(p1){
+//     console.log(p1);
+
+// }
+
+// soma("Nunes")
+// soma("1998")
+// soma("7.9")
+
+function soma(n1=0, n2=0){
+    console.log(n1+n2);
+}
+
+// soma(10,10)
+
+soma(10)
