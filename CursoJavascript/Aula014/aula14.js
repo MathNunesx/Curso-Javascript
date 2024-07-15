4ad74f488b9d95bb9b3eee4882f9ecf2a3ca3a5f
Matheus Nunes <matheusjobs04@gmail.com>
2024-07-15 18:43:33 -0300

    aula 14

---
+++ CursoJavascript/Aula014/aula14.js
@@ -0,0 +1,14 @@
+// função aninhada
+
+const soma = (...valores)=>{
+    const somar = val => {
+        let res = 0;
+        for(v of val){
+            res += v
+            return res
+        }
+    }
+    return somar(valores)
+}
+
+console.log(soma(10,10,10));
