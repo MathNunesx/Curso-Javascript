38ff0be0ed2effeaa7ae3a984aff6870ed81a476
Matheus Nunes <matheusjobs04@gmail.com>
2024-08-03 17:50:04 -0300

    create e remove element

--- CursoJavascript/Aula023/Aula23.js
+++ CursoJavascript/Aula023/Aula23.js
@@ -1,4 +1,21 @@
 // CREATE ELEMENT   
+// const caixa = document.querySelector("#caixa");
+// const btn = [...document.querySelectorAll(".curso")]
+// const cursos = ["html", "css", "php", "js", "react", "mysql"]
+
+// cursos.map((el, chave) => {
+//     const novo_elemento = document.createElement("div");
+
+//     novo_elemento.setAttribute("id", "c"+chave+1)
+//     novo_elemento.setAttribute("class", "curso")
+//     novo_elemento.innerHTML = el;
+
+//     caixa.appendChild(novo_elemento)
+
+// })
+
+// REMOVE ELEMENT
+
 const caixa = document.querySelector("#caixa");
 const btn = [...document.querySelectorAll(".curso")]
 const cursos = ["html", "css", "php", "js", "react", "mysql"]
@@ -9,10 +26,14 @@ cursos.map((el, chave) => {
     novo_elemento.setAttribute("id", "c"+chave+1)
     novo_elemento.setAttribute("class", "curso")
     novo_elemento.innerHTML = el;
+    novo_elemento.addEventListener("click", (evt)=>{
+        caixa.removeChild(evt.target)
+    })
 
     caixa.appendChild(novo_elemento)
 
 })
+
 
 
 
